506174144c3a34967c657a4542be14bcae30c404
Dmytro Pryshliak <dmytropryshliak@DPRYSHLIAK3-MAC.local>
2023-11-22 17:36:58 +0200

    Word component updates

--- src/components/Word/index.tsx
+++ src/components/Word/index.tsx
@@ -1,19 +1,19 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import Icon from "../Icon";
 import { IconsNames, IconsSizes } from "../Icon/types";
 import "./styles/index.css";
 import { WordProps } from "./types";
 
 const Word: FC<WordProps> = ({ word }) => {
-    const link = `https://www.macmillandictionary.com/dictionary/british/${word}`;
-    return (
-        <a className="word" target="_blank" href={link}>
-            {word}
-            <div className="word-icon">
-                <Icon name={IconsNames.RightArrow} size={IconsSizes.Large} />
-            </div>
-        </a>
-    )
-}
+  const link = `https://dictionary.cambridge.org/dictionary/english/${word}`;
+  return (
+    <a className="word" target="_blank" href={link} rel="noreferrer">
+      {word}
+      <div className="word-icon">
+        <Icon name={IconsNames.RightArrow} size={IconsSizes.Large} />
+      </div>
+    </a>
+  );
+};
 
 export default Word;
