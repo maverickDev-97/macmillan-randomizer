fdede26ef7d2b95790982b5bfdae3f7b5c3b5fc0
Dmytro Pryshliak <dmytropryshliak@DPRYSHLIAK3-MAC.local>
2023-11-23 18:37:12 +0200

    Refactored navigation styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./styles/index.css";
 
 export const Header = () => {
+  const { pathname } = useLocation();
+
+  const isMainPage = !pathname.includes("/saved");
+
   return (
     <header className="header">
       <nav>
         <ul className="menu">
-          <li className="menu__item">
+          <li className={`menu__item ${isMainPage ? "active" : ""}`}>
             <Link to="/">Randomizer</Link>
           </li>
-          <li className="menu__item">
+          <li className={`menu__item ${!isMainPage ? "active" : ""}`}>
             <Link to="/saved">Saved words</Link>
           </li>
         </ul>
